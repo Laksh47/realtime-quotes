72aef3ac8c0f77435a2c65dd60923128ca2fa8f6
Kaiser Dandangi <kaiser.dandangi@gmail.com>
2020-12-24 11:25:49 -0500

    use watchlist as default tab
    and some scaffolding to make it user selectable

--- src/App.js
+++ src/App.js
@@ -6,9 +6,25 @@ import MarketSummary from "./pages/MarketSummary";
 import Watchlist from "./pages/Watchlist";
 
 class App extends React.Component {
+  constructor() {
+    this.state = {
+      defaultActiveTabIndex: this.getDefaultTabPreference()
+    };
+  }
+  
+  updateDefaultTabPreference = (defaultActiveTabIndex) => {
+    this.setState({ defaultActiveTabIndex });
+    console.log("### [TODO] persist tab settings to localStorage...");
+  }
+  
+  getDefaultTabPreference() {
+    console.log("### [TODO] fetch tab settings from localStorage...");
+    return 0;
+  }
+  
   render() {
     return (
-      <Tabs>
+      <Tabs defaultActiveTabIndex={this.state.defaultActiveTabIndex} handleSetDefaultTab={this.updateDefaultTabPreference}>
         <Tab tabName={"Market Summary"}>
           <MarketSummary />
         </Tab>
